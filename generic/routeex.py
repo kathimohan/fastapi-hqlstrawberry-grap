748b3dd9df4530b7712e2ebd936e1b29b22e849b
mohan <mohangoud@sivisoft.com>
2025-07-28 09:27:00 +0530

    msg

--- generic/routeex.py
+++ generic/routeex.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from your_app.models import Customer
 from your_app.routes.generator import generate_crud_routes
 
+create 5 fast apis -- create , update, partial_update, delete and filter_delete  . Actually it should be one call from backend side, there backend coder pass only table name , api router tag, and what to return for create instead of whole instance, the coder pass list which should return ; if not passed any return all elements of db class ; partial_upadte - coder pass which elements to allow for show in swagger page, delete expects id in swagger page, filter_delete expects column_name and value
+
 app = FastAPI()
 
 # Custom path prefix: /client/...
